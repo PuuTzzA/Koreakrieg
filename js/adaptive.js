7d54e434de6f34d549990f6ff531861d7d0c3b24
PuuTzzA <101471317+PuuTzzA@users.noreply.github.com>
2022-06-10 16:55:00 +0200

    Update adaptive.js

--- js/adaptive.js
+++ js/adaptive.js
@@ -30,11 +30,18 @@ let width_factor = cal_width / 1920;
 
 document.getElementsByTagName("html")[0].style.fontSize = String(cal_height * (10 / 1080)) + "px";
 
-window.onresize = () =>{
-    if(window.innerHeight > height * 1.1 || window.innerHeight < height * 0.9 ||
-        window.innerWidth > width *1.1 || window.innerWidth < width *0.9){
-            location.reload(); 
+window.onresize = () => {
+    if (x.matches) { // If media query matches
+        console.log("small device");
+
+    } else {
+        if (window.innerHeight > height * 1.1 || window.innerHeight < height * 0.9 ||
+            window.innerWidth > width * 1.1 || window.innerWidth < width * 0.9) {
+            location.reload();
+        }
     }
 }
 
 console.log('%c PuuTzzA', 'font-weight: bold; font-size: 50px;color: red; text-shadow: 3px 3px 0 rgb(217,31,38) , 6px 6px 0 rgb(226,91,14) , 9px 9px 0 rgb(245,221,8) , 12px 12px 0 rgb(5,148,68) , 15px 15px 0 rgb(2,135,206) , 18px 18px 0 rgb(4,77,145) , 21px 21px 0 rgb(42,21,113)');
+
+var x = window.matchMedia("(max-width: 700px)")
